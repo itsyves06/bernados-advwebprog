c11c610b86233af86e5bb61de3499ae7bdab3eee
Reinilyn Bernados <reinilyn0306@gmail.com>
2026-03-19 00:01:46 +0800

    .

---
+++ bernados-client/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: Reinilyn Bernados<br />
+          Email: bernadosrz1@students.national-u.edu.ph<br />
+          Other Personal Info:<br/>
+          <br></br>
+          Age: 22 <br/>
+          Birthday: March 6, 2026<br/>
+          <br></br>
+          Likes: <br/>
+          - Books<br/>
+          - Music<br/>
+          - Flowers<br/>
+          <br></br>
+          Hobbies: <br/>
+          - reading<br/>
+          - listening to music<br/>
+          -watching movies and series<br/>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
